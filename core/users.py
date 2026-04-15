0848b7b8afedd8bab40e2c421069b3368c54426a
president <mrroshan8349@gmail.com>
2026-04-15 10:11:34 +0530

    Update users.py

--- core/users.py
+++ core/users.py
@@ -1,1 +1,30 @@
+import requests
 
+HEADERS = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+def extract_users(chat_link):
+    try:
+        res = requests.get(chat_link + "/participants", headers=HEADERS)
+        data = res.json()
+
+        denorm = data.get("denormalized", {})
+        key = chat_link + "/participants"
+
+        items = denorm.get(key, {}).get("data", {}).get("items", [])
+
+        users = []
+
+        for u in items:
+            uid = u.split("-")[-1]
+            user_data = denorm.get(u, {}).get("data", {})
+
+            users.append({
+                "user_id": uid,
+                "image": user_data.get("look_image")
+            })
+
+        return users
+    except:
+        return []
